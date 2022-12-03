092bb7a9af20c951e449de1e4b48e8ea09f472a4
Sergey <yvarovsg@icloud.com>
2022-12-03 22:23:34 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by AlkoFitoNiashka on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
